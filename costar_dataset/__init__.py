6b305178de2147c6351d70f576b5e21733d75906
Andrew Hundt <ATHundt@gmail.com>
2018-12-03 15:55:28 -0500

    init.py micro version bump

--- costar_dataset/__init__.py
+++ costar_dataset/__init__.py
@@ -1,5 +1,5 @@
 """costar_dataset - Code for the The CoSTAR Block Stacking Dataset  includes a real robot trying to stack colored children's blocks. https://sites.google.com/site/costardataset"""
 
-__version__ = '0.4.0'
+__version__ = '0.4.0.1'
 __author__ = 'Andrew Hundt <ATHundt@gmail.com>'
 __all__ = []
